87d82571eccd0a357dee8cf16ae2b5e994dbfcd1
nelil21 <nelilamaani21@gmail.com>
2024-10-02 10:07:46 +0700

    tugas 5

--- plantshop/settings.py
+++ plantshop/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
 

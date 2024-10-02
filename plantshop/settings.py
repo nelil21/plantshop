7026b52c727a5866eebfc67ea8ec16fccc3db08f
nelil21 <nelilamaani21@gmail.com>
2024-10-02 13:17:49 +0700

    selesai tugas indvidu 5

--- plantshop/settings.py
+++ plantshop/settings.py
@@ -121,10 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Misalkan ini adalah folder yang digunakan saat collectstatic
 
+
+STATIC_URL = '/static/'
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static' # merujuk ke /static root project pada mode development
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 
 # Default primary key field type
